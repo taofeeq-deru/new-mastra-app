2ca56301743f870ec70586b30fe043ad2381dcc9
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2025-02-07 17:23:36 +0100

    remove candidate workflow

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,10 +1,9 @@
 import { Mastra, createLogger } from "@mastra/core";
 import { weatherWorkflow } from "./workflows";
 import { weatherAgent } from "./agents";
-import { candidateWorkflow } from "./workflows/ai-resume-workflow";
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow, candidateWorkflow },
+  workflows: { weatherWorkflow },
   agents: { weatherAgent },
   logger: createLogger({
     level: "info"
